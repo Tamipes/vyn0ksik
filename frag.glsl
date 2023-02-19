bd43712bf8bea6f1c0eb0b0400d46646beececb5
Tamipes <tamipesagniyah@gmail.com>
2023-02-19 17:58:13 +0100

    Yugamu

--- frag.glsl
+++ frag.glsl
@@ -20,6 +20,43 @@ float hash(in vec2 xy, in float seed) {
     return clamp(fract(tan(distance(xy * PHI, xy) * mod(seed, 1000.0)) * xy.x) * 1.003921568627, 0.0, 1.0);
 }
 
+float noise(in vec2 x) {
+    vec2 p = floor(x);
+    vec2 f = fract(x);
+    f = f * f * (3.0 - 2.0 * f);
+
+    float a = hash(p + vec2(0, 0), 2018.2023);
+    float b = hash(p + vec2(1, 0), 2018.2023);
+    float c = hash(p + vec2(0, 1), 2018.2023);
+    float d = hash(p + vec2(1, 1), 2018.2023);
+    return mix(mix(a, b, f.x), mix(c, d, f.x), f.y);
+}
+
+const mat2 mtx = mat2(0.80, 0.60, -0.60, 0.80);
+float complex_noise(in vec2 p) {
+    float f = 0.0;
+
+    f += 0.5000 * noise(p);
+    p = mtx * p * 2.0;
+    f += 0.2500 * noise(p);
+    p = mtx * p * 2.0;
+    f += 0.1250 * noise(p);
+    p = mtx * p * 2.0;
+    f += 0.0625 * noise(p);
+
+    return f / 0.9375;
+}
+
+float pattern(in vec2 p, out vec2 q, out vec2 r) {
+    q.x = complex_noise(p + vec2(-0.31415, 0.0));
+    q.y = complex_noise(p + vec2(7.2, -7.4));
+
+    r.x = complex_noise(p + 4.0 * q + vec2(10.95, 4.14));
+    r.y = complex_noise(p + 4.0 * q + vec2(-E, -PHI));
+
+    return complex_noise(p + 4.0 * r);
+}
+
 void main() {
     vec2 uv = gl_FragCoord.xy / uResolution.xy;
 
@@ -30,10 +67,12 @@ void main() {
     // gl_FragColor = vec4((1.0 - t) * c1 + t * c2, 1.0);
     // //gl_FragColor = vec4(uv.x, uv.y, 0.0, 1.0);
 
-    vec4 h;
-    for(int i = 0; i < 32; i++) {
-        h = vec4(vec3(hash(gl_FragCoord.xy, uTime)), 1.0);
-    }
+    const vec3 c1 = vec3(0.831, 0.686, 0.216);
+    const vec3 c2 = vec3(0.753, 0.753, 0.753);
 
-    gl_FragColor = h;
+    vec2 q, r;
+    float p = pattern((gl_FragCoord.xy * 0.035) + vec2(2018.0, 2023.0), q, r);
+    float n = complex_noise(gl_FragCoord.xy * 0.035 + vec2(2018.0, 2023.0));
+
+    gl_FragColor = vec4(mix(mix(c1, c2, n) * p, c1, (q.x + r.y + n) * 0.333), 1.0);
 }
